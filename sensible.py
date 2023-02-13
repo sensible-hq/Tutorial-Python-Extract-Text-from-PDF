a15d64398a272ea9be6434a6052b193f9b1ce378
Michael Nyamande <nyamandemike43@gmail.com>
2023-02-13 21:54:05 +0200

    initial commit

---
+++ sensible.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+URL = "https://api.sensible.so/v0/extract/invoices?environment=development"
+# Your pdf file path
+DOCUMENT_PATH = "test.pdf"
+# Your Sensible API key
+API_KEY = "INSERT YOUR KEY HERE"
+
+headers = {
+     'Authorization': 'Bearer {}'.format(API_KEY),
+    'Content-Type': 'application/pdf'
+}
+with open(DOCUMENT_PATH, 'rb') as pdf_file:
+    body = pdf_file.read()
+response = requests.request(
+    "POST",
+    URL,
+    headers=headers,
+    data=body)
+try:
+    response.raise_for_status()
+except requests.RequestException:
+    print(response.text)
+else:
+    print(json.dumps(response.json(), indent=2))
