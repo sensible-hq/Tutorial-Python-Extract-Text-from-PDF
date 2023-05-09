ebbc9dfd1fc0c3abc1d2231f9fab9d7d0d95120c
Michael Nyamande <nyamandemike43@gmail.com>
2023-05-09 09:04:56 +0200

    minor changes

--- sensible.py
+++ sensible.py
@@ -5,10 +5,10 @@ URL = "https://api.sensible.so/v0/extract/invoices?environment=development"
 # Your pdf file path
 DOCUMENT_PATH = "test.pdf"
 # Your Sensible API key
-API_KEY = "INSERT YOUR KEY HERE"
+SENSIBLE_API_KEY = "INSERT YOUR KEY HERE"
 
 headers = {
-     'Authorization': 'Bearer {}'.format(API_KEY),
+     'Authorization': 'Bearer {}'.format(SENSIBLE_API_KEY),
     'Content-Type': 'application/pdf'
 }
 with open(DOCUMENT_PATH, 'rb') as pdf_file:
